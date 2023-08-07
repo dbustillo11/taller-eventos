6d7cb733db5901d2463b3eb88e807cf1e79f4dcb
dbustillo11 <bustillo.contacto@gmail.com>
2023-08-07 13:55:48 -0300

    Funcionando
    Los estilos están en index.html, en caso de requerirlos en un archivo de estilos.css favor solicitar

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const divBtn = document.getElementById("div-btn");
+  
+    divBtn.addEventListener("click", function () {
+      alert("Hola! Soy el div");
+    });
+  });
